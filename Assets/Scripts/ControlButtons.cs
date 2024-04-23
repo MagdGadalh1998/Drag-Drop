993c2bc8e00f76edfd7280ea8e1cd72cf237f738
MagdGadalh1998 <96065052+MagdGadalh1998@users.noreply.github.com>
2024-04-23 11:10:19 +0200

    first

---
+++ Assets/Scripts/ControlButtons.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class ControlButtons : MonoBehaviour
+{
+    public Button[] btns;
+    private const string GameEntryCountKey = "GameEntryCount";
+
+    private void Start()
+    {
+        int gameEntryCount = PlayerPrefs.GetInt(GameEntryCountKey, 0) + 1;
+        PlayerPrefs.SetInt(GameEntryCountKey, gameEntryCount);
+
+        Debug.Log("Before Game Entry Count: " + PlayerPrefs.GetInt(GameEntryCountKey));
+        switch (gameEntryCount)
+        {
+            case 1:
+                ControlButton(btns[0]);
+                break;
+            case 2:
+                ControlButton(btns[1]);
+                break;
+            case 3:
+                ControlButton(btns[2]);
+                break;
+            case 4:
+                ControlButton(btns[3]);
+                PlayerPrefs.SetInt(GameEntryCountKey, 0);
+                Debug.Log("When Finish =" + PlayerPrefs.GetInt(GameEntryCountKey));
+                break;
+        }
+    }
+    void ControlButton(Button _btn)
+    {
+        foreach(Button btn in btns)
+        {
+            btn.interactable = false;
+        }
+        _btn.interactable = true;
+
+    }
+    void Update()
+    {
+        
+    }
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+}
---
+++ Assets/Scripts/ReturnButtonHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnButtonHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnReturnButtonClick()
+    {
+        SceneManager.LoadScene(0);
+/*
+        // Update button interactability in ButtonController script
+        ControlButtons buttonController = FindObjectOfType<ControlButtons>();
+        buttonController.EnableNextButton();*/
+    }
+}
